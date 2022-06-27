c624c6c1b457ba1c662a979afc2c16a69526db94
unknown <kaustavpaul12310@gmail.com>
2022-06-28 00:13:50 +0530

    fourth  commit

--- demo.js
+++ demo.js
@@ -1,1 +1,8 @@
-console.log('i hate git');
+
+//document.title='kp123';
+var headerTitle=document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.innerText='good';//it does  consider style tag and do not diplay text under span 
+//headerTitle.textContent='ola';//it does not consider style tag and  diplay text under span 
+//headerTitle.innerHTML='<h3>hi</h3>';
+headerTitle.style.borderBottom='solid 3px #000';
