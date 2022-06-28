344c4ae5057aeece056f1d76b419fefd06b0a89f
unknown <kaustavpaul12310@gmail.com>
2022-06-28 14:21:58 +0530

    7th  commit

--- demo.js
+++ demo.js
@@ -25,10 +25,75 @@
 {
     items[i].style.fontWeight='bold';
 }*/
-var li=document.getElementsByTagName('li');
-console.log(li);
-for(let i=0;i< li.length ;i++)
+//var li=document.getElementsByTagName('li');
+//console.log(li);
+/*for(let i=0;i< li.length ;i++)
 {
     li[i].style.backgroundColor='red';
+}*/
+
+/////////////////////////// querySelector
+/*
+var items=document.querySelector('.list-group-item');//by default it takes the first li text,but it does not throw a error like getElementsByClassName.
+//console.log(items);
+items.style.backgroundColor='#ccc'; //by default it changes the first li text
+
+var header=document.querySelector('#main-header');
+header.style.borderBottom= ' solid 7px #111';
+
+var input = document.querySelector('input');
+input.value='kaustav paul';//it only change the first i/p
+
+var submit= document.querySelector('input[type="submit"]');
+submit.value='SEND';
+
+//var item= document.querySelector('.list-group-item');
+//item.style.color='red';//by default it change the first li item
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color='blue';//last li text is changed
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='yellow';//it changes the 2nd element of li
+
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)')
+thirdItem.style.color='#fff';
+*/
+//  QUERYSELECTORALL
+
+var titles = document.querySelectorAll('.title');
+//console.log(titles);//two items we have here
+
+titles[0].textContent = 'Hello';//it changes the first element
+
+var odd= document.querySelectorAll('li:nth-child(odd)');//it takes the odd li
+var even=document.querySelectorAll('li:nth-child(even)');//it takes the even li
+
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor="green";
 }
+
+var secondChild=document.querySelectorAll('.list-group-item');
+console.log(secondChild);
+secondChild[1].style.color="red";
+
+/*var secondChild=document.querySelectorAll('li:nth-child(2)');
+console.log(secondChild);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
