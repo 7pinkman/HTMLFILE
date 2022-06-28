c63a1dc238014bc8e3f36c7941427d0bfeda9f9b
unknown <kaustavpaul12310@gmail.com>
2022-06-28 11:42:40 +0530

    6th  commit

--- demo.js
+++ demo.js
@@ -6,8 +6,8 @@
 //headerTitle.textContent='ola';//it does not consider style tag and  diplay text under span 
 //headerTitle.innerHTML='<h3>hi</h3>';
 //headerTitle.style.borderBottom='solid 3px #000';
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
+//var items=document.getElementsByClassName('list-group-item');
+//console.log(items);
 //items[1].textContent='hi';
 //items[1].style.fontWeight = 'bold';
 //items[1].style.backgroundColor ='yellow';
@@ -19,9 +19,16 @@ console.log(items);
     items[i].style.backgroundColor='#f4f4f4';
 }
 */
-items[2].style.backgroundColor='green';
+//items[2].style.backgroundColor='green';
 
-for(var i=0; i< items.length ; i++)
+/*for(var i=0; i< items.length ; i++)
 {
     items[i].style.fontWeight='bold';
+}*/
+var li=document.getElementsByTagName('li');
+console.log(li);
+for(let i=0;i< li.length ;i++)
+{
+    li[i].style.backgroundColor='red';
 }
+
