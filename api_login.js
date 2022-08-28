8e5d5797e8d7afb966f4bfa18c10e059b3199f77
unknown <kaustavpaul12310@gmail.com>
2022-08-28 09:31:26 +0530

    rest_get_dynamic_page  commit

---
+++ api_login.js
@@ -0,0 +1,96 @@
+var submitButton=document.getElementById('submit');
+/*document.addEventListener("DOMContentLoaded", () => {
+    localStorage.getItem();
+});*/
+
+
+
+/*
+window.addEventListener("DOMContentLoaded", () => {
+    const localStorageObj = localStorage;
+    const localstoragekeys  = Object.keys(localStorageObj)
+
+    for(var i =0; i< localstoragekeys.length; i++){
+        const key = localstoragekeys[i]
+        const userDetailsString = localStorageObj[key];
+        console.log(userDetailsString);
+        const userDetailsObj = JSON.parse(userDetailsString);
+        showNewUserOnScreen(userDetailsObj);
+    }
+});
+*/
+function showNewUserOnScreen(user)
+{
+    const parentNode=document.getElementById('listofpeople');
+    const childHtml=`<li id = ${user.emailId}> ${user.nameId} - ${user.emailId}
+                        <button onclick=deleteUser('${user.emailId}') > Delete User </button>
+                        <button onclick=editUser('${user.emailId}','${user.nameId}') >Edit User </button>
+                        </li>`;
+    parentNode.innerHTML=parentNode.innerHTML+childHtml;
+                        
+}
+
+function deleteUser(emailId)
+{
+    localStorage.removeItem(emailId);
+    removeFromScreen(emailId);
+}
+function removeFromScreen(emailId)
+{
+    const parentNode=document.getElementById('listofpeople');
+    const childNode=document.getElementById(emailId);
+    parentNode.removeChild(childNode);
+}
+
+function editUser(emailId,nameId)
+{
+    //console.log(JSON.parse(localStorage.getItem(emailId)).nameId);
+    localStorage.removeItem(emailId);
+    document.getElementById('name').value=nameId;
+    document.getElementById('email').value=emailId;
+    const parentNode=document.getElementById('listofpeople');
+    const childNode=document.getElementById(emailId);
+    parentNode.removeChild(childNode);
+    
+
+    
+}
+
+
+submitButton.addEventListener("click", (e) => {
+
+    e.preventDefault();
+    var nameId=document.getElementById('name').value;
+    var emailId=document.getElementById('email').value;
+
+    if(nameId.length > 0 && emailId.length >0 )
+    {
+        const obj={
+            nameId:nameId,
+            emailId:emailId
+        };
+        //localStorage.setItem(emailId , JSON.stringify(obj));
+        //console.log(nameId);
+       // addNewLineElement(obj);
+       axios.post("https://crudcrud.com/api/733b391797f1461fb189186fe3abc419/appointmentData",obj)
+       .then((responce) => { 
+        console.log(responce);//check the responce in console
+        showNewUserOnScreen(responce.data);
+       })
+       .catch((err) => {
+        console.log(err);
+       })
+       //showNewUserOnScreen(obj);
+    }
+    //console.log(nameId.value);
+    
+});
+
+function addNewLineElement(obj)
+{
+    const ul=document.getElementById('listofpeople');
+    const li=document.createElement('li');
+    li.appendChild(document.createTextNode(obj.nameId + '    '+obj.emailId));
+    ul.appendChild(li);
+   // console.log(obj.nameId + '    '+obj.emailId);
+}
